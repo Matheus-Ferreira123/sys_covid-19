730c6a4a3b052f2be605a6fb9d6cc42c6f2674ee
matheus-ferreira <matheus8142@hotmail.com>
2022-11-24 21:59:05 -0300

    finalizando o sistema

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include <unistd.h>
 #include <locale.h>
 #include <conio.h>
+#include <time.h>
 /*nome, CPF, telefone, endereço (rua, número, bairro,
 cidade, estado e CEP), data de nascimento e e-mail, data do diagnóstico e, se for o caso,
 informar existentes comorbidades (diabetes, obesidade, hipertensão, tuberculose etc.)*/
 
 FILE *listaPacientes;
+FILE *grupoRisco;
 
 typedef struct endereco Endereco ;
 struct endereco
@@ -37,7 +39,7 @@ struct paciente
 	char email[40];
 	Data diagnostico;
 	char comorbidades[60];
-	int idade[40];
+	int idade;
 }paciente;
 
 typedef struct usuario Usuario;
@@ -103,7 +105,8 @@ int telaMenu(){
 
     printf(" 1 - CADASTRAR PACIENTES.\n");
     printf(" 2 - CONSULTAR PACIENTES CADASTRADOS.\n");
-    printf(" 3 - CONSULTAR PACIENTES COM COMORBIDADES.\n\n\n");
+    printf(" 3 - CONSULTAR PACIENTES DO GRUPO DE RISCO.\n\n");
+    printf (" 0 - SAIR.\n\n");
     printf(" Selecione uma das opcoes para continuar: ");
     scanf("%s", &opcao);
 
@@ -113,13 +116,23 @@ int telaMenu(){
     }else if(opcao == '2'){
         telaConsulta();
     }else if(opcao == '3'){
-        telaComorbidade();
+        telaConsultaRisco();
+    }else if(opcao == '0'){
+        telaCreditos();
     }else{
         limparTela();
         printf("\n\n OPÇÃO INCORRETA");
         sleep(1);
         limparTela();
         telaMenu();
+    }
+}
+void abrir_arqRisco(){
+    grupoRisco = fopen("grupo_de_risco.txt", "a+b");
+    if(grupoRisco == NULL){
+        printf("Erro na abertura do arquivo\n");
+        system("pause");
+        exit(1);
     }
 }
 
@@ -142,11 +155,48 @@ void telaConsulta(){
     fclose(listaPacientes);
 
     abrir_arq();
+    printf("\n 0 - voltar");
     printf("\n\nInforme o cpf do paciente para emitir a ficha: ");
     scanf("%s", &cpf);
 
+    if(igual(cpf, "0") == 1){
+        fclose(listaPacientes);
+        telaMenu();
+    }
+
     buscarPacientePorCpf(cpf);
     fclose(listaPacientes);
+}
+
+void buscarPacientePorCpfRisco(cpf){
+    int a=0;
+
+    fseek(grupoRisco,SEEK_SET,1);
+    fread(&paciente,sizeof(paciente),1,grupoRisco);
+
+    if(igual(paciente.cpf, cpf) == 1){
+        a++;
+        mostrarFichaRisco(paciente);
+    }
+
+    while(!feof(grupoRisco)){
+        if(igual(paciente.cpf, cpf) == 1){
+            a++;
+            printf("CPF: %s -", paciente.cpf);
+            printf(" Nome: %s\n", paciente.nome);
+            mostrarFichaRisco(paciente);
+        }
+
+        fread(&paciente,sizeof(paciente),1,grupoRisco);
+    }
+    if(a == 0){
+        printf("\n Paciente não encontrado!");
+        sleep(2);
+        telaConsultaRisco();
+    }
+
+
+
 }
 
 void buscarPacientePorCpf(cpf){
@@ -171,9 +221,8 @@ void buscarPacientePorCpf(cpf){
         fread(&paciente,sizeof(paciente),1,listaPacientes);
     }
     if(a == 0){
-        printf("Paciente não encontrado\n");
-        fclose(listaPacientes);
-        sleep(3);
+        printf("\n Paciente não encontrado!");
+        sleep(2);
         telaConsulta();
     }
 
@@ -200,11 +249,70 @@ int mostrarFicha(Paciente paciente){
     printf(" Rua: %s\n", paciente.endereco.rua);
     printf(" Número: %s\n", paciente.endereco.numero);
     printf("\nData do Diagnóstico: %s / %s / %s\n", paciente.diagnostico.dia, paciente.diagnostico.mes, paciente.diagnostico.ano);
+    printf("\n Comorbidades: %s", paciente.comorbidades);
     printf("\n\n0 - Voltar\n");
-    printf("\nSelecione uma das opcoes para continuar: ");
+    printf("\nSelecione a opção para continuar: ");
     scanf("%s", opcao);
 
     telaConsulta();
+}
+
+void telaConsultaRisco(){
+    system("cls");
+    char cpf[20];
+
+    imprimirCabecalho();
+    abrir_arqRisco();
+    listarPacientesRisco();
+    fclose(grupoRisco);
+
+    abrir_arqRisco();
+    printf("\n 0 - voltar");
+    printf("\n\nInforme o cpf do paciente para emitir a ficha: ");
+    scanf("%s", &cpf);
+
+    if(igual(cpf, "0") == 1){
+        fclose(grupoRisco);
+        telaMenu();
+    }
+
+    buscarPacientePorCpfRisco(cpf);
+    fclose(grupoRisco);
+}
+
+int mostrarFichaRisco(){
+    char opcao[40] = "";
+    limparTela();
+    imprimirCabecalho();
+
+    printf(" Nome: %s\n", paciente.nome);
+    printf(" CPF: %s\n", paciente.cpf);
+    printf(" Idade: %d\n", paciente.idade);
+    printf(" CEP: %s\n", paciente.endereco.cep);
+
+    printf("\n\n0 - Voltar\n");
+    printf("\nSelecione a opção para continuar: ");
+    scanf("%s", opcao);
+
+    telaConsulta();
+
+}
+
+void listarPacientesRisco(){
+    int a=0;
+
+    fseek(grupoRisco,SEEK_SET,1);
+    fread(&paciente,sizeof(paciente),1,grupoRisco);
+
+    printf("Pacientes encontrados: \n\n");
+    while(!feof(grupoRisco)){
+        a++;
+
+        printf(" CPF: %s - ", paciente.cpf);
+        printf("Nome: %s\n", paciente.nome);
+
+        fread(&paciente,sizeof(paciente),1,grupoRisco);
+    }
 }
 
 void listarPacientes(){
@@ -228,7 +336,8 @@ void listarPacientes(){
 int telaCadastro(){
 
     char nomeArquivo[40];
-    char temComorbidades[40];
+    int temComorbidades = 0;
+    Paciente pacienteRisco;
 
     limparTela();
     imprimirCabecalho();
@@ -241,7 +350,8 @@ int telaCadastro(){
     strcpy(nomeArquivo, paciente.nome);
     strcat(nomeArquivo, ".txt");
 
-    abrir_arq(nomeArquivo);
+    abrir_arqRisco();
+    abrir_arq();
 
     printf(" CPF: ");
     fflush(stdin);
@@ -267,6 +377,7 @@ int telaCadastro(){
     printf(" Ano: ");
     fflush(stdin);
     gets(paciente.dataNascimento.ano);
+    paciente.idade = calcIdade(paciente.dataNascimento.dia, paciente.dataNascimento.mes, paciente.dataNascimento.ano);
 
     printf(" \nDados de endereço: \n");
     printf(" CEP: ");
@@ -308,18 +419,26 @@ int telaCadastro(){
     fflush(stdin);
     gets(paciente.diagnostico.ano);
 
-    printf(" Contêm comorbidades? (S|N) ");
-    fflush(stdin);
-    gets(temComorbidades);
+    printf(" Contêm comorbidades? (1 = Sim| 2 = Não) ");
+    scanf("%i", &temComorbidades);
 
-    if(igual(temComorbidades, "S") == 1 || igual(temComorbidades, "S") == 1){
+    if(temComorbidades == 1){
         printf(" Qual(is): ");
         scanf("%s", &paciente.comorbidades);
+
+        if(paciente.idade > 65 && temComorbidades == 1){
+            fseek(grupoRisco,SEEK_END,1);
+            fwrite(&paciente,sizeof(paciente),1,grupoRisco);
+        }
+
+        fclose(grupoRisco);
     }
 
     fseek(listaPacientes,SEEK_END,1);
     fwrite(&paciente,sizeof(paciente),1,listaPacientes);
     fclose(listaPacientes);
+
+
 
     limparTela();
     imprimirCabecalho();
@@ -328,7 +447,49 @@ int telaCadastro(){
     telaMenu();
 }
 
-    //Tela de pessoas com comorbidades
+int calcIdade(diaNasc, mesNasc, anoNasc) {
+    time_t mytime;
+    mytime = time(NULL);
+    struct tm tm = *localtime(&mytime);
+    int year_diff;
+
+    int dia, mes, ano;
+
+    dia = tm.tm_mday;
+    mes = tm.tm_mon + 1;
+    ano = tm.tm_year + 1900;
+
+    diaNasc = atoi(diaNasc);
+    mesNasc = atoi(mesNasc);
+    anoNasc = atoi(anoNasc);
+
+    if(dia < diaNasc){
+        if (mes == 3){
+            if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)){
+                dia += 29;
+            }else{
+                dia += 28;
+            }
+        }else if (mes == 5 || mes == 7 || mes == 10 || mes == 12){
+           dia += 30;
+        }else{
+           dia += 31;
+        }
+
+        mes = mes - 1;
+    }
+
+    if (mes < mesNasc){
+        mes += 12;
+        ano -= 1;
+    }
+
+
+    year_diff = ano - anoNasc;
+    return year_diff;
+}
+
+//Tela de pessoas com comorbidades
 int telaComorbidade(){
 
     limparTela();
@@ -338,7 +499,7 @@ int telaComorbidade(){
     telaMenu();
 }
 
-    //Tela de pessoas cadastradas
+//Tela de pessoas cadastradas
 int telaCadastrado(){
 
     limparTela();
@@ -349,7 +510,10 @@ int telaCadastrado(){
 }
 
 int telaCreditos(){
-    printf("UNIP PIM IV 2022");
+    limparTela();
+    printf(" \n\n\n\n    UNIP PIM IV 2022");
+    sleep(1);
+    exit(0);
 }
 
 void imprimirCabecalho(){
